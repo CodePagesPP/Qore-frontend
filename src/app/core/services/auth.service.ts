80608e4b18b22937b73244df17c516b4c1ef41f0
espinozaje <jeisson12aaron@gmail.com>
2025-08-07 11:56:39 -0500

    login with settoken

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,8 +1,22 @@
 import { Injectable } from '@angular/core';
-
+import { environment } from '../../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { AuthRequest, AuthResponse } from '../models/auth.model';
+import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  
+  private apiUrl = `${environment.apiUrl}/auth`;
+
+  constructor(private http: HttpClient) {}
+
+  login(credentials: AuthRequest): Observable<AuthResponse> {
+    return this.http.post<AuthResponse>(`${this.apiUrl}/login`, credentials);
+  }
+
+  isAuthenticated(): boolean {
+    return !!localStorage.getItem('token');
+  }
+
 }
--- src/app/features/auth/login/login.component.ts
+++ src/app/features/auth/login/login.component.ts
@@ -1,11 +1,31 @@
 import { Component } from '@angular/core';
+import { AuthRequest } from '../../../core/models/auth.model';
+import { AuthService } from '../../../core/services/auth.service';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login.component',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  credentials: AuthRequest = { email: '', password: '' };
+  error: string | null = null;
 
+  constructor(private authService: AuthService, private router: Router) {}
+
+  onSubmit(): void {
+    this.authService.login(this.credentials).subscribe({
+      next: (res) => {
+        localStorage.setItem('token', res.token);
+        this.router.navigate(['/dashboard']); 
+      },
+      error: (err) => {
+        this.error = 'Credenciales incorrectas';
+      },
+    });
+  }
 }
